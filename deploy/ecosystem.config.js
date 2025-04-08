9dad6a157b087e1c62ec93811d7513ffaaff08de
ramirolepori <hito.arg.it@gmail.com>
2025-04-07 23:48:23 -0300

    Primer commit

---
+++ deploy/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: "thermostat-backend",
+        script: "./dist/index.js",
+        cwd: "/home/pi/thermostat-mvp/backend",
+      },
+    ],
+  };
+  
